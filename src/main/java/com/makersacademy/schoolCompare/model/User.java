80376bc03103f72640755efd3ca0435c3c30ee85
God'smercy Oluwanusin <gmercyoluwanusin@gmail.com>
2024-12-10 16:35:57 +0000

    bug fix

--- src/main/java/com/makersacademy/schoolCompare/model/User.java
+++ src/main/java/com/makersacademy/schoolCompare/model/User.java
@@ -11,11 +11,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-<<<<<<< HEAD
-
-    private boolean enabled;
-=======
->>>>>>> 8de5d321d5902199913837f8a547f8192f02923f
 
     public User() {
 
@@ -27,26 +22,5 @@ public class User {
 
     public User(String username, boolean enabled) {
         this.username = username;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 }
