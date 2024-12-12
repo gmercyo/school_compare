c2ea6513a85ef1828969a2c12c60e3b9f8efc48d
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-12-12 15:56:43 +0000

    added some more to the seedfile

--- schools_seed.sql
+++ schools_seed.sql
@@ -41,7 +41,7 @@ VALUES (
     'primary',
     'mixed',
     'n/a',
-    'community',
+    'coe',
     'https://www.stjohnevangelist.co.uk');
 INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
 VALUES (
@@ -99,3 +99,87 @@ VALUES (
     'n/a',
     'community',
     'https://www.egaschool.co.uk/');
+INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
+VALUES (
+    'Betty Layward Primary School',
+    'Clissold Road, N16 9EX',
+    -0.0846212,
+    51.5578995,
+    '[{"year": "''24-''25", "radius": 0.305}, {"year": "''23-''24", "radius": 0.339}, {"year": "''22-''23", "radius": 0.38}, {"year": "''21-''22", "radius": 0.381}, {"year": "''20-''21", "radius": 0.352}]',
+    3,
+    'https://files.ofsted.gov.uk/v1/file/50233922',
+    'primary',
+    'mixed',
+    'n/a',
+    'community',
+    'https://www.bettylayward.hackney.sch.uk/');
+INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
+VALUES (
+    'Grasmere Primary School',
+    '92 Albion Road, N16 9PD',
+    -0.0835736,
+    51.5550003,
+    '[{"year": "''24-''25", "radius": 0.331}, {"year": "''23-''24", "radius": 0.297}, {"year": "''22-''23", "radius": 0.417}, {"year": "''21-''22", "radius": 0.38}, {"year": "''20-''21", "radius": 0.3}]',
+    3,
+    'https://files.ofsted.gov.uk/v1/file/50183475',
+    'primary',
+    'mixed',
+    'n/a',
+    'community',
+    'https://www.grasmere.hackney.sch.uk/');
+INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
+VALUES (
+    'Grazebrook Primary School',
+    'Lordship Road, N16 0QP',
+    -0.0831442,
+    51.5642014,
+    '[{"year": "''24-''25", "radius": 0.439}, {"year": "''23-''24", "radius": 0.283}, {"year": "''22-''23", "radius": 0.557}, {"year": "''21-''22", "radius": 0.599}, {"year": "''20-''21", "radius": 0.41}]',
+    4,
+    'https://files.ofsted.gov.uk/v1/file/50235291',
+    'primary',
+    'mixed',
+    'n/a',
+    'community',
+    'http://www.grazebrook.hackney.sch.uk');
+INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
+VALUES (
+    'St. Mary''s Church of England Primary School',
+    'Barn Street, N16 0JT',
+    -0.0815231,
+    51.5623016,
+    '[{"year": "''24-''25", "radius": -1}, {"year": "''23-''24", "radius": -1}, {"year": "''22-''23", "radius": -1}, {"year": "''21-''22", "radius": -1}, {"year": "''20-''21", "radius": -1}]',
+    3,
+    'https://files.ofsted.gov.uk/v1/file/50220293',
+    'primary',
+    'mixed',
+    'n/a',
+    'coe',
+    'https://st-marys.hackney.sch.uk/');
+INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
+VALUES (
+    'William Patten Primary School',
+    'Stoke Newington Church Street, N16 0NX',
+    -0.0749634,
+    51.5617981,
+    '[{"year": "''24-''25", "radius": 0.422}, {"year": "''23-''24", "radius": 0.365}, {"year": "''22-''23", "radius": 0.207}, {"year": "''21-''22", "radius": 0.475}, {"year": "''20-''21", "radius": 0.227}]',
+    4,
+    'https://files.ofsted.gov.uk/v1/file/50233486',
+    'primary',
+    'mixed',
+    'n/a',
+    'community',
+    'http://www.williampatten.hackney.sch.uk');
+INSERT INTO schools (name, address, longitude, latitude, catchment_radius, ofsted_rating, ofsted_url, type, gender, sen_availability, religious_affiliation, website)
+VALUES (
+    'Stoke Newington School and Sixth Form',
+    'Clissold Road, N16 9EX',
+    -0.0846212,
+    51.5578995,
+    '[{"year": "''24-''25", "radius": 0.797}, {"year": "''23-''24", "radius": 0.911}, {"year": "''22-''23", "radius": 0.9}, {"year": "''21-''22", "radius": 0.603}, {"year": "''20-''21", "radius": 0.747}]',
+    3,
+    'https://files.ofsted.gov.uk/v1/file/50192955',
+    'secondary',
+    'mixed',
+    'n/a',
+    'community',
+    'http://www.stokenewingtonschool.co.uk/');
