1f46e5449fdfa3b9098a4baca5e2c18d6d66d59c
Tobi Elewa-Gidado <tobi.elewa@hotmail.co.uk>
2024-12-12 10:32:14 +0000

    Edited font to CamelCase

--- src/main/java/com/makersacademy/schoolCompare/model/Answer.java
+++ src/main/java/com/makersacademy/schoolCompare/model/Answer.java
@@ -10,22 +10,22 @@ public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long school_id;
-    private Long user_id;
-    private Long question_id;
+    private Long schoolId;
+    private Long userId;
+    private Long questionId;
     private String role;
     private String content;
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     public Answer() {}
 
-    public Answer(Long school_id, Long user_id, Long question_id, String role, String content, LocalDateTime created_at) {
-        this.school_id = school_id;
-        this.user_id = user_id;
-        this.question_id = question_id;
+    public Answer(Long schoolId, Long userId, Long questionId, String role, String content, LocalDateTime createdAt) {
+        this.schoolId = schoolId;
+        this.userId = userId;
+        this.questionId = questionId;
         this.role = role;
         this.content = content;
-        this.created_at = created_at;
+        this.createdAt = createdAt;
     }
 
 
@@ -34,15 +34,15 @@ public class Answer {
     }
 
     public Long getSchool_id() {
-        return school_id;
+        return schoolId;
     }
 
     public Long getUser_id() {
-        return user_id;
+        return userId;
     }
 
     public Long getQuestion_id() {
-        return question_id;
+        return questionId;
     }
 
     public String getRole() {
@@ -54,23 +54,23 @@ public class Answer {
     }
 
     public LocalDateTime getCreated_at() {
-        return created_at;
+        return createdAt;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setSchool_id(Long school_id) {
-        this.school_id = school_id;
+    public void setSchool_id(Long schoolId) {
+        this.schoolId = schoolId;
     }
 
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+    public void setUser_id(Long userId) {
+        this.userId = userId;
     }
 
-    public void setQuestion_id(Long question_id) {
-        this.question_id = question_id;
+    public void setQuestion_id(Long questionId) {
+        this.questionId = questionId;
     }
 
     public void setRole(String role) {
@@ -81,8 +81,8 @@ public class Answer {
         this.content = content;
     }
 
-    public void setCreated_at(LocalDateTime created_at) {
-        this.created_at = created_at;
+    public void setCreated_at(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
 
--- src/main/java/com/makersacademy/schoolCompare/model/Question.java
+++ src/main/java/com/makersacademy/schoolCompare/model/Question.java
@@ -11,18 +11,18 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long school_id;
-    private Long user_id;
+    private Long schoolId;
+    private Long userId;
     private String content;
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     public Question() {}
 
-    public Question(Long school_id, Long user_id, String content, LocalDateTime created_at) {
-        this.school_id = school_id;
-        this.user_id = user_id;
+    public Question(Long schoolId, Long userId, String content, LocalDateTime createdAt) {
+        this.schoolId = schoolId;
+        this.userId = userId;
         this.content = content;
-        this.created_at = created_at;
+        this.createdAt = createdAt;
 
     }
 
@@ -30,40 +30,40 @@ public class Question {
     return id;
     }
 
-    public Long getSchool_id() {
-    return school_id;
+    public Long getSchoolId() {
+    return schoolId;
     }
 
-    public Long getUser_id() {
-        return user_id;
+    public Long getUserId() {
+        return userId;
     }
 
     public String getContent(){
         return content;
     }
 
-    public LocalDateTime getCreated_at(){
-        return created_at;
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
     }
 
     public void setId(Long id){
         this.id = id;
     }
 
-    public void setSchool_id(Long school_id){
-        this.school_id = school_id;
+    public void setSchool_id(Long schoolId){
+        this.schoolId = schoolId;
     }
 
-    public void setUser_id(Long user_id){
-        this.user_id = user_id;
+    public void setUser_id(Long userId){
+        this.userId = userId;
     }
 
     public void setContent(String content){
         this.content = content;
     }
 
-    public void setCreated_at(LocalDateTime created_at){
-        this.created_at = created_at;
+    public void setCreated_at(LocalDateTime createdAt){
+        this.createdAt = createdAt;
     }
 }
 
