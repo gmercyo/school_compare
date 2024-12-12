56cb2516a988616c80997b80baca9b3a7c41dafc
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-12-12 14:02:51 +0000

    added new question form template and associated controller method

--- src/main/java/com/makersacademy/schoolCompare/model/User.java
+++ src/main/java/com/makersacademy/schoolCompare/model/User.java
@@ -11,16 +11,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @Column(name = "auth0_id")
+    private String auth0Id;
 
-    public User() {
-
-    }
-
-    public User(String username) {
+    public User(String username, String auth0Id) {
         this.username = username;
+        this.auth0Id = auth0Id;
     }
+    public User() {}
 
-    public User(String username, boolean enabled) {
-        this.username = username;
-    }
+    public Long getId() { return id; }
+    public String getUsername() { return username; }
+    public String getAuth0Id() { return auth0Id; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setUsername(String username) { this.username = username; }
+    public void setAuth0Id(String auth0Id) { this.auth0Id = auth0Id; }
 }
--- src/main/java/com/makersacademy/schoolCompare/repository/QuestionRepository.java
+++ src/main/java/com/makersacademy/schoolCompare/repository/QuestionRepository.java
@@ -1,9 +1,9 @@
 package com.makersacademy.schoolCompare.repository;
 
 import com.makersacademy.schoolCompare.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends CrudRepository<Question, Long> {
 }
