96400770742d39c825ce6bb1d562dd4719b17574
God'smercy Oluwanusin <gmercyoluwanusin@gmail.com>
2024-12-17 12:15:43 +0000

    updating QuestionLike repository and Controller

--- src/main/java/com/makersacademy/schoolcompare/repository/QuestionLikeRepository.java
+++ src/main/java/com/makersacademy/schoolcompare/repository/QuestionLikeRepository.java
@@ -4,6 +4,11 @@ import com.makersacademy.schoolcompare.model.QuestionLike;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface QuestionLikeRepository extends CrudRepository<QuestionLike, Long> {
+    Optional<QuestionLike> findByUserIdAndQuestionId(Long userId, Long questionId);
+    List<QuestionLike> findByUserId(Long userId);
 }
